772f1b8364fdf2f5e24a4b6a8e109787f3d0c6ca
Niklas Scholz <niklas.scholz@outlook.com>
2024-12-01 21:02:09 +1100

    better code
    Signed-off-by: Niklas Scholz <niklas.scholz@outlook.com>

---
+++ 1/solve_2.py
@@ -0,0 +1,20 @@
+list_a = []
+dict_b = {}
+
+with open("1/input.txt", "r") as file:
+    for row in file:
+        chunks = row.strip().split(" ")
+        list_a.append(int(chunks[0]))
+
+        list_b_chunk = int(chunks[-1])
+
+        dict_b[list_b_chunk] = dict_b.get(list_b_chunk, 0) + 1
+
+
+res = 0
+
+for i in list_a:
+    if i in dict_b:
+        res += i * dict_b[i]
+
+print(res)
