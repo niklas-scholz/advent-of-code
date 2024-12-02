459345f2b0957754b2f60728c2de63a27709733d
Niklas Scholz <niklas.scholz@outlook.com>
2024-12-03 07:25:47 +1100

    cleanup
    Signed-off-by: Niklas Scholz <niklas.scholz@outlook.com>

--- python/day_2/solve_3.py
+++ python/day_2/solve_3.py
@@ -1,5 +1,3 @@
-# list_a, list_b = [], []
-
 reports = []
 
 with open("input_day_2.txt", "r") as file:
@@ -7,14 +5,10 @@ with open("input_day_2.txt", "r") as file:
         reports.append([int(level) for level in row.split(" ")])
 
 
-print(reports)
-
 res = 0
 
 for report in reports:
     slope = 0
-
-    print("new row")
 
     for i in range(len(report) - 1):
         cur_level = report[i]
@@ -24,7 +18,6 @@ for report in reports:
         print(diff)
 
         if abs(diff) > 3 or abs(diff) < 1:
-            print("diff not valid")
             break
 
         if diff > 0:
@@ -38,32 +31,8 @@ for report in reports:
             elif slope == -1:
                 break
 
-        print("valid so far")
-
         if i + 1 == len(report) - 1:
             res += 1
 
-        # if cur_level -
-        #
-        # cur
-        #
-        # if
-
-    # for level in :
-    #     slope = 0
 
 print(res)
-#
-# for level in level
-
-# assert len(w) == len(list_b)
-#
-# list_a.sort()
-# list_b.sort()
-#
-# res = 0
-#
-# for i in range(len(list_a)):
-#     res += abs(list_a[i] - list_b[i])
-#
-# print(res)
